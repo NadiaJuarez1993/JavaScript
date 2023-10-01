eb4577e77a901e24e3ae7904c5efbe940878a528
Nadia Juarez <nadiajuarezz@hotmail.com>
2023-10-01 12:35:43 -0300

    Clase 3 FUNCIONES NATIVAS

---
+++ CLASE 3 FUNCIONES NATIVAS/index.js
@@ -0,0 +1,84 @@
+/*REPASO
+FUNCION: BLOQUE DE CODIGO QUE SE PUEDE DEFINIR Y PREPARADO PARA USAR
+ 
+que tengo y que necesito?
+una funcion tiene que hacer una sola cosa y la tiene que hacer bien
+
+
+
+scope:alcance de mi codigo
+
+codigo inalcansable: todo codigo que este por debajo del return
+*/
+
+
+/*FUNCIONES NATIVAS: funciones que vienen con JV
+Permiten manipular info de diferente manera.*/
+
+const fullname = "Nadia Juarez"
+const number = 30
+
+
+/*length -->saber cuantos caracteres hay en un string  es un metodo. los caracteres empiezan en cero y cuenta espacios
+console.log(fullname.length)*/
+
+/*.toUpperCase=()         toma un string y me lo debuelve todo en mayuscula 
+console.log(fullname.toUpperCase())*/
+
+/*.toLowerCase()---> devuelve el string en minuscula*/
+
+/*slice --> toma un string y me permite cortarloa traves de la pesicion de sus caracterers
+console.log(fullname.slice(5))
+console.log(fullname.slice( 0,5))  si se quiere desde donde hasta donde*/
+
+/*.includes()---> debuelve buleano (true o false)
+.console.log(fullname.includes(x))
+.console.log (fullname.includes(z))*/
+
+
+/*.indexOf() --> devuelce la posicion del caracter que recibe como argumento
+console.log(fullname.indexOf(""))  //preguntamos por el espacio
+console.log(fullname.indexOf("j")) */
+
+/*charAt() --> inversa del indexOf el caracter que esta en la posiciocion
+console.log(fullname.charAt(2))*/
+
+/*replace()--> recibe como primer argumento lo que quiero reemlazar y el segundo por que quieroreemplazar
+console.log(fullname.replace ("nadia", "jaqueline"))*/
+
+/*toString() -- tranforma de nume a string 
+console.log()*/
+
+//////
+/*objeto math --> se pide en la consola que te brinda herramientas */
+
+/*round-->redondea. se pasa al entero mas cercano
+const num =1.6
+console.log(Math.round(num))*/
+
+/*ceil--> redondea hacia arriba
+console.log*/
+
+
+/*floor-->redondea para abajo*/
+
+
+/*random--> devuelve numero aleateorio entre o y 1
+console.log(Math.random())   no llevanumero entre parentesis
+console.log(Math.random() * 10)  si lo multiplica me da un numero random de o a 10 segun el numero que multiplique*/
+
+/*min---> devuelve el numero mas bajo
+console.log(Math.min(5, 8, 7))  pasa el numero mas bajo que leponga entre parentesis*/
+
+
+/*max devuelve el numero mas alto
+console.log(Math.max(5, 8, 7))*/
+
+
+
+
+
+
+
+/*replace*/
+
