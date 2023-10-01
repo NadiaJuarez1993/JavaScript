e9a8d9536a37625beaaab1fb5df407195aff903d
Nadia Juarez <nadiajuarezz@hotmail.com>
2023-10-01 12:12:17 -0300

    Clase 1 Variables

---
+++ CLASE 1 VARIABLES/index.js
@@ -0,0 +1,138 @@
+// js en un lenguaje no typado que permite cambiar el tipo de dato
+
+
+
+
+// console.log("hola mundo")
+// document.write("hola mundo")
+// console.log(10)
+
+// variables, cajita que guarda info
+//  tipos de datos
+// como se declara variable: let , las variables pueden tener un nombre, a eleccion
+
+// lo rojo es el nombre de la  variable
+// antes del =es el nombre de la variable
+// despues del igual=  es el string
+
+// formas de llamar variables let y const
+
+// let firstname = "Nadia" //string
+// let lastname = "Juarez"
+// let age = 30 //number
+// let willDiner = undefined //es un dato que no se sabe si se realiza
+// 
+// let nulo = null // dato nulo
+// let hasPet = true //boolean
+// let knowhowtodrive = false //boolean
+// let notanumber = NaN // NaN
+
+// console.log(firstname)
+// document.write(firstname)
+
+// concatenar variable, a partir de dos variables se pueen mostrar juntas
+
+// console.log(firstname + "" + lastname)
+// literal
+// console.log(`${firstname} ${lastname}`)
+
+// typof sirve para preguntar el tipo de valor y me retorna un string
+// todo adentro de comilla es string
+//  console.log(typeof firstname)
+
+//  let tipoDe = typeof firstname
+//  console.log(typeof tipoDe)
+
+//  let firstname = "Nadia"
+//  console.log(firstname)
+//  firstname = "jaqueline"
+// console.log(firstname)
+
+
+//const = no acepta cambios
+
+// const placeOfBirth = "buenos aires"
+// console.log(placeOfBirth)
+
+//alert("soy un viris")
+//console.log("me ejecute!!!!")
+
+//el confirm--- me va a guardar un boolean con la respuesta del usuario
+//let acceptTermAndCondition = confirm("Aceptas los terminos y condiciones?")
+
+//propmt -- abre una ventana que permite al usuario escribir 
+//se interactua con el usuario
+//si no se completa guarda null
+
+//si el usuario acepta sin completar me devuelve el scrin vacio
+//const username = prompt("ingrresa tu nombre")
+//console.log(` El usuario ingresado es ${username}`)
+
+//siempre guarda un string
+//const age = prompt("ingrese su edad")
+
+//el parseint cambia de string a numero pero pierde coma
+//console.log(parseInt(age))
+
+//pasa de string a numero 
+//console.log(Number(age))
+
+
+//OPERADORES ARITMETICOS0 SUMA RESTAproducto divicion
+
+// const number1 = "20"
+// const number2 = "10"
+
+
+// console.log(number1 + number2)
+
+// console.log(number1 - number2)
+
+// console.log(number1 * number2)
+
+// console.log(number1 / number2)
+
+// console.log(number1 % number2) ///devuelve el resto de la divicion de los dos numeros
+
+// const  number1 = Number (prompt("ingrese el primer numero")) 
+// const number2 = Number (prompt ("ingrese el segundo (numero"))
+
+
+
+
+// console.log( "la suma es:", number1 + number2)
+
+// console.log( "la resta es:", number1 - number2)
+
+// console.log("la multiplicacion es:", number1 * number2)
+
+// const result =  number1 / number2
+// console.log ( "la divicion es:"parseInt(result))
+
+// console.log("la suma es:", number1 % number2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
