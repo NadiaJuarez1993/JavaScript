7add93c07a47fd72c066c0f0edc902f522f0e3f0
Nadia Juarez <nadiajuarezz@hotmail.com>
2023-10-01 12:16:47 -0300

    Actividades de clase 1 Variables

---
+++ CLASE 1 VARIABLES/Actividades/index.js
@@ -0,0 +1,50 @@
+//EJERCICIO 1
+//Declarar variables con datos de un usuario (user, age, email)
+//Mostrar estos datos por consola
+//Mostrar el tipo de dato de cada variable por consola
+
+// let firstname = "Nadia"
+// let lastname = "Juarez"
+// let age = 30
+// let email = "nadiajuarezz@hotmail.com"
+
+// console.log( typeof firstname)
+// console.log( typeof lastname)
+// console.log(typeof  age)
+// console.log(typeof  email)
+
+//EJERCICIO 2
+//Pedir al usuario sus datos personales (nombre, edad, pais, profesion)
+// const username = prompt("Ingrese su nombre y apellido")
+// console.log(` El usuario ingresado es ${username}`)
+
+const age = prompt("Ingrese su edad")
+console.log(`La edad ingresada es ${age}`)
+
+// const placeOfBirth = prompt ("Ingrese su lugar de nacimiento")
+// console.log( `El lugar de naciemiento del usuario es ${placeOfBirth}`)
+
+// const profession = prompt ("Ingrese su profesion")
+// console.log(`La profesion del usuario es  ${profession}`)
+//Mostrar esos datos por consola usando concatenacion
+
+// console.log(`${username} ${age} ${placeOfBirth} ${profession}`)
+
+
+//Mostrar por consola la edad + 10
+console.log(Number(age))
+
+
+const number1 = Number(age)
+const number2 = 10
+const number3 = 1
+
+console.log(number1 + number2 + number3)
+
+
+
+//Pedir al usuario dos numeros y mostrar por el documento los resultados de cada calculo aritmetico. Ejemplo: El resultado de la suma es: ${resultado}
+// const  number1 = Number (prompt("ingrese el primer numero")) 
+// const number2 = Number (prompt ("ingrese el segundo numero"))
+
+// console.log( "la suma es:", number1 + number2)
