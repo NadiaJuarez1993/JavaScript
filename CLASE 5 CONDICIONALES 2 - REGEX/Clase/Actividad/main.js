83a28bb7e9358fb7cce3c0c446d21a3655b3542e
Nadia Juarez <nadiajuarezz@hotmail.com>
2023-10-10 20:46:06 -0300

    CLASE 5 ACTIVIDADES

---
+++ CLASE 5 CONDICIONALES 2 - REGEX/Clase/Actividad/main.js
@@ -0,0 +1,9 @@
+/*
+PRACTICA
+
+=> ADIVINA EL NUMERO
+- Crear un juego en el que se le pide al usuario adivinar un numero random del 0 al 10
+- Si el usuario gana, emitir un mensaje que diga felicitaciones adivinaste. Caso contrario, lo siento perdiste.
+- Ahora le subimos la dificultad. Vamos a darle al usuario tres vidas como chances de adivinar. Con cada fracaso debe restarse una vida, en caso de ganar mantener el mensaje de felicidades. Si se queda sin vidas, perdio.
+
+*/
